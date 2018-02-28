99ce518e3487a70a1794ecd41c3a463752032ef4
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2018-02-28 11:07:37 +0000

    adding projects url

--- server.js
+++ server.js
@@ -57,6 +57,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+    });
+});
+
 app.listen(port, () => {
     console.log(`Running on port ${port}`);
 });
+
